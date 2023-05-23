f7292b9c1605be027ba3ce8200f681008dcf406b
ChocoLa-1484 <naiyanyang@gmail.com>
2023-05-23 17:39:10 +0800

    Initial Commit

---
+++ Shared/HoldemApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldemApp.swift
+//  Shared
+//
+//  Created by 楊乃諺 on 2023/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HoldemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
